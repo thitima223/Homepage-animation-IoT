8db94128b2e5b38e7209828ce3045e7380c4668f
0128 Thitima Nawpraya <147297357+thitima223@users.noreply.github.com>
2026-03-08 11:32:30 +0700

    up Animation 1

--- AnimationIoT_1/src/main.tsx
+++ AnimationIoT_1/src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-// import './index.css'  <-- แนะนำให้ใส่ // ข้างหน้า หรือลบออกไปเลยครับ
+import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
